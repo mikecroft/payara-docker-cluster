88e811cddb505350ca7015b26a70fc8ff57cdd72
Mike <backtotheoldhouse@gmail.com>
2015-08-27 22:22:36 +0100

    refactored to use functions. Creating cluster doesn't work yet

--- run-cluster.sh
+++ run-cluster.sh
@@ -22,6 +22,7 @@ set -o nounset                              # Treat unset variables as an error
 
 ASADMIN=/opt/payara41/glassfish/bin/asadmin
 PAYA_HOME=/opt/payara41
+PASSWORD=payara
 
 # Attempt to clean up any old containers
 docker kill das   >/dev/null 2>&1
@@ -34,16 +35,65 @@ docker rm node1   >/dev/null 2>&1
 docker run -i -p 4848:4848 -t -d --name das   payara:4.1.152.1.zulu8  /bin/bash
 docker run -i              -t -d --name node1 payara:4.1.152.1.zulu8  /bin/bash
 
-# Create cluster
-docker exec das $ASADMIN start-domain domain1
+createPasswordFile() {
 
+cat << EOF > pfile
+AS_ADMIN_PASSWORD=$PASSWORD
+AS_ADMIN_SSHPASSWORD=$PASSWORD
+EOF
+
+docker cp pfile das:$PAYA_HOME
+docker exec das cat $PAYA_HOME/pfile
+
+}
+
+
+enableSecureAdmin() {
+
+# Set admin password
+docker exec das $ASADMIN start-domain domain1
+    
 docker exec das curl  -X POST \
-    -H 'X-Requested-By: YeaGlassFish' \
+    -H 'X-Requested-By: payara' \
     -H "Accept: application/json" \
     -d id=admin \
     -d AS_ADMIN_PASSWORD= \
-    -d AS_ADMIN_NEWPASSWORD=glassfish \
+    -d AS_ADMIN_NEWPASSWORD=$PASSWORD \
     http://localhost:4848/management/domain/change-admin-password
-
-docker exec das $ASADMIN --user admin --passwordfile pwdfile enable-secure-admin
+    
+docker exec das $ASADMIN --user admin --passwordfile $PAYA_HOME/pfile enable-secure-admin
 docker exec das $ASADMIN restart-domain domain1
+
+}
+
+
+createSSHNodeCluster() {
+
+# Create cluster SSH node
+docker exec das $ASADMIN --user admin --passwordfile=$PAYA_HOME/pfile --port 4848 create-cluster cluster
+docker exec das $ASADMIN --user admin --passwordfile=$PAYA_HOME/pfile --port 4848 setup-ssh --generatekey=true node1
+
+# FIXME setup-ssh doesn't work yet
+if [ $? -ne 0 ]
+then
+    echo "couldn't setup SHH"
+else
+    docker exec das $ASADMIN --user admin --passwordfile=$PAYA_HOME/pfile --port 4848 create-node-ssh --nodehost node1 --sshuser payara --installdir '/opt/payara41' node1
+    docker exec das $ASADMIN --user admin --passwordfile=$PAYA_HOME/pfile --port 4848 create-instance --node localhost.localdomain --cluster cluster instance0
+    docker exec das $ASADMIN --user admin --passwordfile=$PAYA_HOME/pfile --port 4848 create-instance --node node1                 --cluster cluster instance1
+fi
+
+}
+
+createConfigNodeCluster() {
+
+docker exec das $ASADMIN --user admin --passwordfile=$PAYA_HOME/pfile --port 4848 create-cluster cluster
+docker exec das $ASADMIN --user admin --passwordfile=$PAYA_HOME/pfile --port 4848 create-node-config --nodehost node1 --installdir $PAYA_HOME node1
+
+# TODO finish function
+
+}
+
+createPasswordFile
+enableSecureAdmin
+createSSHNode
